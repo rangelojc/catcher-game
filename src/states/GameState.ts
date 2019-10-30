8baf7f80ba8c2af188514e625a1ba0e2803758ec
Ramce <ramuzuconcepcion@gmail.com>
2019-10-30 11:35:10 +0800

    Added log to state

--- src/states/GameState.ts
+++ src/states/GameState.ts
@@ -1,25 +1,55 @@
 export class State {
-    public gameOver: boolean = false;
-    public score: number = 0;
-    public highscore: number = 0;
-    public life: number = 3;
+    public gameOver: boolean;
+    public score: number;
+    public highscore: number;
+    public life: number;
 
-    public level: number = 1;
+    public level: number;
+    public starSpawnRate: number;
+    public starDropSpeed: number;
+
     public originalStarSpawnRate: number = 1000;
     public originalStarDropSpeed: number = 550;
-    public starSpawnRate: number = 1000;
-    public starDropSpeed: number = 550;
 
     public scoreTier: number[] = [
         0, 5, 10, 15, 20, 30, 50, 60, 70, 80,
         100, 120, 150
     ]
 
+    public log: any;
+
+    constructor() {
+        this.init();
+    }
+
+    public init() {
+        this.gameOver = false;
+        this.score = 0;
+        this.highscore = 0;
+        this.life = 3;
+
+        this.level = 1;
+        this.starSpawnRate = 1000;
+        this.starDropSpeed = 550;
+
+        this.log = [];
+    }
+
     public get(key) {
-        return this[key];
+        const value = this[key];
+        this.logEntry(key, value, "Get");
+        return value;
     }
 
     public set(key, value) {
+        this.logEntry(key, value, "Set");
         this[key] = value;
+    }
+
+    private logEntry(key, value, type) {
+        this.log.push({
+            timestamp: Date.now(),
+            content: `${type} ${key} with value ${value}`
+        });
     }
 }
